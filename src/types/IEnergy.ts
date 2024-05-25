500bfced8d512de03584e7ff78c03bf5ba034b6b
jotacib3 <jotacib3@gmail.com>
2024-05-25 13:16:48 -0400

    feat(types): add IEnergy and IGenerationData interfaces

---
+++ src/types/IEnergy.ts
@@ -0,0 +1,13 @@
+import IGenerationData from "./IGenerationData";
+
+/**
+ * EnergyMix represents the energy generation data for a specific time period.
+ * @property {string} from - The start time of the time period.
+ * @property {string} to - The end time of the time period.
+ * @property {IGenerationData[]} generation - The array of energy generation data for different types of fuels.
+ */
+export interface IEnergy {
+    from: string;
+    to: string;
+    generation: IGenerationData[];
+}
