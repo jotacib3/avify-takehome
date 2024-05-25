83f5f602eac7c7f34879eaa0a9b63cbf50bf30f1
jotacib3 <jotacib3@gmail.com>
2024-05-25 14:02:31 -0400

    feat(app): update to include EnergySection with QueryClientProvider

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import EnergySection from "./components/EnergySection";
 
-const App = () => <h1>UK Energy Mix</h1>;
+// Create a client
+const queryClient = new QueryClient();
+
+/**
+ *  App component is the root component of the application.
+ **/
+const App: React.FC = () => {
+
+    return (
+           <QueryClientProvider client={queryClient}>
+                    <EnergySection />
+           </QueryClientProvider>
+    );
+};
 
 export {
     App
