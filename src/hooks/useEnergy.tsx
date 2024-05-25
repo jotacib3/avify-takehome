2052306f0e0d91626f4c25bbcd64c599549e506a
jotacib3 <jotacib3@gmail.com>
2024-05-25 13:54:00 -0400

    feat(hooks): add useEnergy hook
    - Custom hook that fetches the UK energy mix data from the API

---
+++ src/hooks/useEnergy.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from 'react-query';
+import { IEnergy } from "../types/IEnergy";
+
+/**
+ * useEnergy is a custom hook that fetches the UK energy mix data from the API.
+ * It uses the useQuery hook from react-query for data fetching.
+ */
+const useEnergy = () => {
+    return useQuery<IEnergy[], Error>('energy', async () => {
+        const response = await fetch('https://api.carbonintensity.org.uk/generation');
+        const data = await response.json();
+        return data.data;
+    });
+};
+
+export default useEnergy;
